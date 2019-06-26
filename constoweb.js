11e77002bf1502565b5bc3ae1ec3ea222f3ceb10
dac <dac@dypm.org>
2019-06-26 13:07:18 +0530

    first commit

---
+++ constoweb.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/Emplist', function(req, res){
+	var emplist[
+				firstName:"Minal", lastName:Mudliyar, dept:"development"
+	];
+			  
+read.send(emplist);
+
+});
+app.listen(3000);
+console.log("app listening at port 3000");
